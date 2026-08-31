78b107b4fea32891e48eb851acc0f1a7cbd6fa2d
envbernard <enzovb2020@gmail.com>
2026-08-31 20:30:02 -0300

    arrumando oracle.sh

--- clyvo02aci-oracle.sh
+++ clyvo02aci-oracle.sh
@@ -1,8 +1,6 @@
-# Obter senhas do Storage e do ACR
 STORAGE_KEY=$(az storage account keys list --resource-group rm56300-rg --account-name rm56300storage --query "[0].value" --output tsv)
 ACR_PASSWORD=$(az acr credential show --name rm56300acr --query "passwords[0].value" --output tsv)
 
-# Criar o Container do Banco (ACI) com volume persistente
 az container create \
   --resource-group rm56300-rg \
   --name rm56300-db \
@@ -15,6 +13,7 @@ az container create \
   --registry-password $ACR_PASSWORD \
   --dns-name-label rm56300-db-dns \
   --ports 1521 \
+  --secure-environment-variables ORACLE_PASSWORD="SenhaForte#2026" \
   --azure-file-volume-account-name rm56300storage \
   --azure-file-volume-account-key $STORAGE_KEY \
   --azure-file-volume-share-name oracledata \
